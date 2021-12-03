ae452ce8f8432584ca62bc8205ab53826667a5f2
Abiola Rasheed <rasheedabi97@hotmail.com>
2021-12-03 13:52:37 +0100

    now actually added logging config etc.; added screipt to sync to upgdate server

---
+++ Record.Recorder.Installers/syncToServer.bat
@@ -0,0 +1,4 @@
+scp "Record.Recorder.Installer.De\Release\VinylRecorderInstaller-De.msi" mserver@cadoc:/home/mserver/webserver/files/msi/live/de/ 
+
+scp "Record.Recorder.Installer.En\Release\VinylRecorderInstaller-En.msi" mserver@cadoc:/home/mserver/webserver/files/msi/live/en/ 
+
