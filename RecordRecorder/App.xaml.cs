250b28710abef80090053bc5315f7533607433d7
Abiola Rasheed <rasheedabi97@hotmail.com>
2021-12-03 13:53:28 +0100

    updated version

--- RecordRecorder/App.xaml.cs
+++ RecordRecorder/App.xaml.cs
@@ -15,7 +15,7 @@ namespace RecordRecorder
     /// </summary>
     public partial class App : Application
     {
-        public static Version Version { get; } = new Version("1.0.5");
+        public static Version Version { get; } = new Version("1.0.6");
 
         readonly Logger log = LogManager.GetLogger("fileLogger");
         
