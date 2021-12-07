a2c8cb258addeb636fbc9e1efa640119853c9061
Abiola Rasheed <rasheedabi97@hotmail.com>
2021-12-07 17:43:40 +0100

    automated deployment of release installer

---
+++ Record.Recorder.Installers/syncToServerDe.bat
@@ -0,0 +1,2 @@
+scp "Record.Recorder.Installer.De\Release\VinylRecorderInstaller-De.msi" mserver@cadoc:/home/mserver/webserver/files/msi/live/de/ 
+scp "Record.Recorder.Installer.De\Release\setup.exe" mserver@cadoc:/home/mserver/webserver/files/setup/de/ 
---
+++ Record.Recorder.Installers/syncToServerEn.bat
@@ -0,0 +1,2 @@
+scp "C:\Users\rashe\source\repos\AbiolaR\RecordRecorder\Record.Recorder.Installers\Record.Recorder.Installer.En\Release\VinylRecorderInstaller-En.msi" mserver@cadoc:/home/mserver/webserver/files/msi/live/en/ 
+scp "C:\Users\rashe\source\repos\AbiolaR\RecordRecorder\Record.Recorder.Installers\Record.Recorder.Installer.En\Release\setup.exe" mserver@cadoc:/home/mserver/webserver/files/setup/en/ 
